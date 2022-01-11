10c8b7153bb42c0de1dae134701007fff0208eeb
Alireza Ahmadi <ceo@p30web.org>
2022-01-11 23:29:54 +0330

    clean code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Artisan;
 
 class HomeController extends Controller
 {
